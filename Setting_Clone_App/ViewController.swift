dec1d7cc1479bfb135c4f302d6b96cf4021d1430
study <printfprintf@naver.com>
2022-12-05 18:48:55 +0900

    Initial Commit

---
+++ Setting_Clone_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Setting_Clone_App
+//
+//  Created by bon on 2022/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
